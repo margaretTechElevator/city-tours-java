b9f7032e2a689199abcb0c16c01d984897ff448b
Shannon <shannonsandlin@gmail.com>
2023-08-29 00:50:46 -0500

    login redo

---
+++ vue/src/components/navigation.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="nav">
+    <h1>City Tours</h1>
+    <h3>lets plan the perfect daytrip</h3>
+    <!-- <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+    <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''"
+      >Logout</router-link
+    > -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navigation",
+};
+</script>
+
+<style>
+#nav {
+  border: #fa7c47;
+  border-width: 10px;
+}
+h1 {
+  text-align: center;
+  padding-top: 20px;
+  color: #06445b;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 4rem;
+  font-weight: 900;
+  line-height: 1;
+  text-shadow: 1px 1px 35px rgba(130, 114, 110, 0.584);
+}
+h3 {
+  text-align: center;
+  color: #d04512;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-weight: 700;
+  font-size: 1.5rem;
+  line-height: 0px;
+  padding-bottom: 30px;
+  text-shadow: 1px 1px 10px rgba(251, 249, 248, 0.711);
+}
+</style>
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,71 +1,166 @@
 <template>
-  <div id="login">
-    <form @submit.prevent="login">
-      <h1 >Please Sign In</h1>
-      <div role="alert" v-if="invalidCredentials">
-        Invalid username and password!
-      </div>
-      <div role="alert" v-if="this.$route.query.registration">
-        Thank you for registering, please sign in.
-      </div>
-      <div class="form-input-group">
-        <label for="username">Username</label>
-        <input type="text" id="username" v-model="user.username" required autofocus />
-      </div>
-      <div class="form-input-group">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model="user.password" required />
-      </div>
-      <button type="submit">Sign in</button>
-      <p>
-      <router-link :to="{ name: 'register' }">Need an account? Sign up.</router-link></p>
-    </form>
-  </div>
+  <body>
+    <div id="login">
+      <navigation />
+      <form @submit.prevent="login">
+        <!-- <h1 >City Tours</h1>
+      <h3>lets plan the perfect daytrip</h3> -->
+        <div id="usernameAndPassword">
+          <h2>please sign in</h2>
+          <div role="alert" v-if="invalidCredentials">
+            Invalid username and password!
+          </div>
+          <div role="alert" v-if="this.$route.query.registration">
+            Thank you for registering, please sign in.
+          </div>
+
+          <div class="form-input-group">
+            <!-- <label for="username">Username</label> -->
+            <!-- placeholder="username(email)" -->
+            <input
+              type="text"
+              placeholder="email"
+              class="inputs"
+              id="username"
+              v-model="user.username"
+              required
+              autofocus
+            />
+          </div>
+          <div class="form-input-group">
+            <!-- <label for="password">Password</label> -->
+            <input
+              type="password"
+              placeholder="password"
+              class="inputs"
+              id="password"
+              v-model="user.password"
+              required
+            />
+          </div>
+          <div id="submit">
+            <button type="submit">log in</button>
+          </div>
+          <p></p>
+          <div id="submit">
+            <button @click="$router.push('/register')">
+              join us
+            </button>
+            <!-- <router-link :to="{ name: 'register' }">join</router-link> -->
+          </div>
+        </div>
+      </form>
+    </div>
+  </body>
 </template>
 
 <script>
+import Navigation from "../components/navigation.vue";
 import authService from "../services/AuthService";
 
 export default {
   name: "login",
-  components: {},
+  components: { Navigation },
   data() {
     return {
       user: {
         username: "",
-        password: ""
+        password: "",
       },
-      invalidCredentials: false
+      invalidCredentials: false,
     };
   },
   methods: {
     login() {
       authService
         .login(this.user)
-        .then(response => {
+        .then((response) => {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             this.$router.push("/");
           }
         })
-        .catch(error => {
+        .catch((error) => {
           const response = error.response;
 
           if (response.status === 401) {
             this.invalidCredentials = true;
           }
         });
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
+body {
+  background-image: url();
+}
+button{
+  background-color: rgba(255, 255, 255, 0.74);
+  border: none;
+  text-align: center;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  color: #6b1717;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-weight: 900;
+  font-size: 1rem;
+  line-height: 1;
+  text-shadow: 1px 1px 10px rgba(130, 114, 110, 0.5);
+}
+::placeholder {
+  color: #e0a788e0;
+  font-weight: 900;
+  letter-spacing: 0.15rem;
+  font-size: 0.75rem;
+  background: transparent;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+
 .form-input-group {
   margin-bottom: 1rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  text-align: center;
+  color: #dd5a13;
+  font-weight: 400;
+  letter-spacing: 0.1rem;
+  background: transparent;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
 }
-label {
-  margin-right: 0.5rem;
+
+.inputs {
+  text-align: center;
+  width: 100%;
+  border-color: transparent
+}
+#login {
+  text-align: center;
+  height: 700px;
+  /* background-image: url("https://i.ibb.co/bLCxN8R/vecteezy-young-woman-tourist-looking-at-map-planning-trip-happy-girl-13215197.jpg"); */
+  background-image: url("https://i.ibb.co/svhYTX5/city.png");
+  margin: auto;
+  width: 700px;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+
+h2 {
+  text-align: center;
+  padding-top: 10px;
+  padding-bottom: 15px;
+  color: #6b1717;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-weight: 900;
+  font-size: 1.5rem;
+  line-height: 0;
+  text-shadow: 1px 1px 10px rgba(130, 114, 110, 0.5);
+}
+#usernameAndPassword {
+  margin: auto;
+  width: 30%;
+  background-color: rgba(255, 255, 255, 0.5);
+  padding: 50px;
 }
 </style>
