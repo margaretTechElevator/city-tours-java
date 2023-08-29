077ee92c01b8b3a3601a135b77b7ced87e475a5a
Alexander Stuart <ahdisease@gmail.com>
2023-08-29 17:29:14 -0400

    updated controller methods for landmark objects

--- java/src/main/java/com/techelevator/controller/LandmarkController.java
+++ java/src/main/java/com/techelevator/controller/LandmarkController.java
@@ -2,12 +2,10 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.LandmarkDao;
 import com.techelevator.model.Landmark;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -21,16 +19,35 @@ public class LandmarkController {
         this.dao = dao;
     }
 
-    //todo update with alternate routes other than city
-    @RequestMapping(path = "/landmarks")
-    public List<Landmark> getLandmarks(@RequestParam(value = "city", required = false) String city) {
-        
+    @RequestMapping(path = "/landmarks", method = RequestMethod.GET)
+    public List<Landmark> getLandmarks(@RequestParam(value = "city", required = false) String city,
+                                       @RequestParam(value = "type", required = false) String type) {
+        if ((city == null || city.isBlank()) && (type == null || type.isBlank())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Required arguments: City or Type");
 
-        try {
-            List<Landmark> landmarks = dao.getAllLandmarksByCity(city);
-            return landmarks;
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getLocalizedMessage());
+        } else if (city == null || city.isBlank()) {
+            try {
+                List<Landmark> landmarks = dao.getAllLandmarksByType(type);
+                return landmarks;
+            } catch (Exception e) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getLocalizedMessage());
+            }
+        } else if (type == null || type.isBlank()) {
+            try {
+                List<Landmark> landmarks = dao.getAllLandmarksByCity(city);
+                return landmarks;
+            } catch (Exception e) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getLocalizedMessage());
+            }
         }
+        List<Landmark> landmarks = dao.getAllLandmarksOfTypeByCity(city,type);
+        return landmarks;
+    }
+
+    @RequestMapping(path = "landmarks/{placeId}", method = RequestMethod.GET)
+    public Landmark getLandmarkByPlaceId(@PathVariable String placeId) {
+        Landmark landmark = dao.getLandmarkByPlaceId(placeId);
+
+        return landmark;
     }
 }
--- java/src/main/java/com/techelevator/dao/LandmarkDao.java
+++ java/src/main/java/com/techelevator/dao/LandmarkDao.java
@@ -4,11 +4,12 @@ import com.techelevator.model.Landmark;
 import java.util.List;
 
 public interface LandmarkDao {
+    List<Landmark> getAllLandmarksOfTypeByCity(String city, String type);
     List<Landmark> getAllLandmarksByCity(String city);
 
     List<Landmark> getAllLandmarksByType(String type);
 
     Landmark getLandmarkByPlaceId(String placeId);
 
-    boolean addLandmarkToDatabase();
+    boolean addLandmarkToDatabase(Landmark newLandmark);
 }
