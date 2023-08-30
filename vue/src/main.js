4b474e80c3acbd993a5e7558c9e21fd7844a8262
Alexander Stuart <ahdisease@gmail.com>
2023-08-30 01:10:45 -0400

    experimenting with maps

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps,
+  {
+    load: {
+      key: 'API_KEY_HERE',
+      libraries: 'places'
+    }
+  })
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
 
