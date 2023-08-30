aef86b2b311adf4db1f0e75aaf887b98d6cb3005
kubra <kubracetin.ist@gmail.com>
2023-08-30 16:10:21 -0400

    edded map component

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <div id="app">
+   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
+
     <router-view />
+    <map-component/>
+    
   </div>
+  
 </template>
+<script>
+import MapComponent from './components/MapComponent.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    MapComponent,
+   
+  }
+}
+</script>
