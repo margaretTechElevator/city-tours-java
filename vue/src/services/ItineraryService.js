b3eebd5cc1ddb432f938250b359b3a9628973709
kubra <kubracetin.ist@gmail.com>
2023-08-31 15:58:30 -0400

    added service

---
+++ vue/src/services/ItineraryService.js
@@ -0,0 +1,9 @@
+// import axios from 'axios';
+
+// export default {
+
+//     // add itinerary for authenticated user
+//     addItinerary(itinerary){
+//         return axios.post('/itineraries', itinerary )
+//     }
+// }
