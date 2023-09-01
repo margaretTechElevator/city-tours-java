b0ccb957ff7ac321edb03356cc74c5a9d2b8a43a
Shannon <shannonsandlin@gmail.com>
2023-08-31 20:25:01 -0500

    cant figure out radio buttons on the google maps vue file

--- vue/src/components/GoogleMap.vue
+++ vue/src/components/GoogleMap.vue
@@ -10,21 +10,23 @@
           placeholder="starting address"
           type="input"
           class="inputStartingMiles"
+          id="startingAddress"
         />
         <input
           v-model="radiusInput"
-          placeholder="how many miles are you willing to travel"
+          placeholder="search radius in miles"
           type="input"
           class="inputStartingMiles"
+          id="milesFrom"
         />
         <p></p>
-        <label for="attractionType">what would you like to search</label>
+        <label for="attractionType" id="whatToSearch">things to do</label>
         <div id="attractionTypesRadio" v-for="type in attractionTypes" :key="type">
           <input type="radio" :value="type" v-model="selectedTypes" />
           {{ type }}
         </div>
 
-        <button v-on:click="addToList">let's go!</button>
+        <button v-on:click="addToList" id="letsGo">let's go!</button>
       </div>
 
 
@@ -299,14 +301,14 @@ export default {
 </script>
   
   <style>
-#grid-container {
+/* #grid-container {
   display: grid;
   grid-template-columns: 1fr;
   grid-template-areas:
     "inputs "
     "directions "
     "map ";
-}
+} */
 
 #map {
   grid-area: map;
@@ -328,19 +330,85 @@ export default {
   border: rgb(28, 153, 11) 3px solid;
   margin: auto;
   width: 50%;
+  
 }
 .inputStartingMiles{
   width:300px;
   margin-top:10px;
   margin-left: auto;
   margin-right:auto;
+  text-align: center;
+  width: 100%;
+  box-shadow: 1px 1px 10px rgba(255, 255, 255, 0.36);
+  border:rgb(203, 203, 203) 0.5px solid;
+  background-color: rgba(158, 158, 158, 0.248);
 
 }
 #attractionTypesRadio{
   border: black 3px solid;
+  grid-area: radio;
+  
+  
+
 }
 .grid{
   display:grid;
-  grid-template-columns: ;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-areas: 
+  "startingAddress startingAddress startingAddress startingAddress"
+  "milesFrom milesFrom milesFrom milesFrom"
+  " . whatToSearch whatToSearch ."
+  "radio radio radio radio"
+  ". letsGo letsGo .";
+ 
 }
+#milesFrom{
+  grid-area: milesFrom;
+}
+#startingAddress{
+  grid-area: startingAddress;
+}
+#whatToSearch{
+  grid-area: whatToSearch;
+  text-align: center;
+  padding-top: 20px;
+  padding-bottom: 15px;
+  color: #6b1717;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-weight: 900;
+  font-size: 1.5rem;
+  line-height: 0;
+  text-shadow: 1px 1px 10px rgba(130, 114, 110, 0.5);
+}
+#letsGo{
+  grid-area: letsGo;
+
+}
+button {
+  background-color: rgb(236, 191, 93);
+  border: none;
+  text-align: center;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  color: #6b1717;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-weight: 900;
+  font-size: 1rem;
+  line-height: 1;
+  box-shadow: 1px 1px 10px rgba(130, 114, 110, 0.186);
+
+}
+::placeholder {
+  color: #e0a788e0;
+  font-weight: 900;
+  letter-spacing: 0.15rem;
+  font-size: 0.75rem;
+  background: transparent;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+
+
+  
+
 </style>
