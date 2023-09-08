97c1baa255d5bcc1009be9ff05221e031386e67d
Sam Warner <its.sam.warner@gmail.com>
2023-09-08 14:25:20 -0400

    Only makes whitespace changes in PlanNewTourPage and MyToursPage components

--- vue/src/views/MyToursPage.vue
+++ vue/src/views/MyToursPage.vue
@@ -1,16 +1,13 @@
 <template>
-    <body>
+  <body>
     <Navigation />
-  <h4>my tours</h4>
+    <h4>my tours</h4>
     <TripsTable />
+    <div id="myToursTableBackground"></div>  
+  </body>
+</template>
   
-  <div id="myToursTableBackground">
-
-  </div>  
-    </body>
-  </template>
-  
-  <script scoped>
+<script scoped>
   import Navigation from '../components/Navigation.vue'
   import TripsTable from '../components/TripsTable.vue'
 
@@ -18,38 +15,30 @@
     components: { Navigation, TripsTable },
     name: 'MyToursPage'
   }
-  </script>
+</script>
   
-  <style>
-
-  body{
+<style>
+  body {
     background: url("https://i.ibb.co/TKxNQrR/ny-Opacity.png");
-
-    
-
   }
-  #myToursTableBackground{
+
+  #myToursTableBackground {
     background-color: #fffefe;
     width: auto;
     height:900px;
   }
 
-
-
-h4{
-  margin:auto;
+  h4 {
+    margin:auto;
     width: 80%;
     text-align: center;
     text-shadow: 1px 1px 15px rgba(215, 255, 53, 0.277);
 
-    /* width:175px; */
-      font-size: 2.5rem;
-      font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-      font-weight: 900;
-      color:rgb(1, 37, 105);
-      padding-top:20px;
-      padding-bottom: 10px;
-      
-}
-  
-  </style>
+    font-size: 2.5rem;
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-weight: 900;
+    color:rgb(1, 37, 105);
+    padding-top:20px;
+    padding-bottom: 10px;  
+  }
+</style>
--- vue/src/views/PlanNewTourPage.vue
+++ vue/src/views/PlanNewTourPage.vue
@@ -1,31 +1,25 @@
 <template>
-    <body>
+  <body>
     <Navigation />
-
-      
-  <google-map id="mapElement"/>
+    <google-map id="mapElement"/>
     <TripsTable />
- <route/>
-
-
-
-    </body>
-  </template>
+    <route/>
+  </body>
+</template>
   
-  <script>
+<script>
   import route from '../components/route.vue'
   import GoogleMap from '../components/GoogleMap.vue';
   import Navigation from '../components/Navigation.vue'
   import TripsTable from '../components/TripsTable.vue';
 
   export default {
-    components: { Navigation, GoogleMap, TripsTable, route },
-      name: 'PlanNewTourPage',
-  
+    components: { 
+      Navigation, 
+      GoogleMap, 
+      TripsTable, 
+      route 
+    },
+    name: 'PlanNewTourPage',
   }
-  </script>
-  
-  <style >
-  
-  
-  </style>
+</script>
