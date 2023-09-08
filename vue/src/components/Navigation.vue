f244cb0028bf970a0ffd84bed143e4bea221b73a
Sam Warner <its.sam.warner@gmail.com>
2023-09-08 14:25:43 -0400

    Only makes whitespace changes in Navigation.vue

--- vue/src/components/Navigation.vue
+++ vue/src/components/Navigation.vue
@@ -21,162 +21,162 @@
 </template>
     
     
-    <script>
-export default {
-  components: {},
-  name: "Navigation",
-};
+<script>
+  export default {
+    name: "Navigation",
+  };
 </script>
     
-    <style>
-     a{
+<style>
+  a { 
     font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
       color:#06445b;
       font-weight: 900;
       text-decoration: none;
-      font-size: 1rem;
-      
+      font-size: 1rem;  
   }
-  a:hover{
+
+  a:hover {
     color: #ffffff;
     text-shadow: 1px 1px 15px rgba(66, 66, 67, 0.252);
-
   }
-#city {
-  padding-top: 30px;
-  color: #06445b;
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-  font-size: 4rem;
-  font-weight: 900;
-  text-shadow: 1px 1px 35px rgb(252, 189, 0);
-  font-size: 2.5rem;
-  margin-top: 0px;
-  margin-bottom: 0px;
-  line-height: 8px;
-}
-#daytrip {
-  text-align: start;
-  color: #ffffffdb;
-  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
-  font-weight: 700;
-  font-size: 1rem;
-  text-shadow: 1px 1px 10px rgba(71, 51, 41, 0.501);
-  padding-left: 10px;
-  padding-right: 10px;
-}
 
-.top-nav {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  background-color: #00baf0;
-  background: linear-gradient(to left, #f46b45, #eea849);
-  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-  color: #fff;
-  height: 50px;
-  padding: 1em;
-}
+  #city {
+    padding-top: 30px;
+    color: #06445b;
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 4rem;
+    font-weight: 900;
+    text-shadow: 1px 1px 35px rgb(252, 189, 0);
+    font-size: 2.5rem;
+    margin-top: 0px;
+    margin-bottom: 0px;
+    line-height: 8px;
+  }
 
-.menu {
-  display: flex;
-  flex-direction: row;
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  
+  #daytrip {
+    text-align: start;
+    color: #ffffffdb;
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+    font-weight: 700;
+    font-size: 1rem;
+    text-shadow: 1px 1px 10px rgba(71, 51, 41, 0.501);
+    padding-left: 10px;
+    padding-right: 10px;
+  }
 
-}
-
-.menu > li {
-  margin: 0 1rem;
-  overflow: hidden;
-}
-
-.menu-button-container {
-  display: none;
-  height: 100%;
-  width: 30px;
-  cursor: pointer;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-#menu-toggle {
-  display: none;
-}
-
-.menu-button,
-.menu-button::before,
-.menu-button::after {
-  display: block;
-  background-color: #fff;
-  position: absolute;
-  height: 4px;
-  width: 30px;
-  transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
-  border-radius: 2px;
-}
-
-.menu-button::before {
-  content: "";
-  margin-top: -8px;
-}
-
-.menu-button::after {
-  content: "";
-  margin-top: 8px;
-}
-
-#menu-toggle:checked + .menu-button-container .menu-button::before {
-  margin-top: 0px;
-  transform: rotate(405deg);
-}
-
-
-
-#menu-toggle:checked + .menu-button-container .menu-button::after {
-  margin-top: 0px;
-  transform: rotate(-405deg);
-}
-
-@media (max-width: 700px) {
-  .menu-button-container {
+  .top-nav {
     display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #00baf0;
+    background: linear-gradient(to left, #f46b45, #eea849);
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    color: #fff;
+    height: 50px;
+    padding: 1em;
   }
+
   .menu {
-    position: absolute;
-    top: 0;
-    margin-top: 50px;
-    left: 0;
-    flex-direction: column;
-    width: 100%;
-    align-items: end;
-  }
-  #menu-toggle ~ .menu li {
-    height: 0;
+    display: flex;
+    flex-direction: row;
+    list-style-type: none;
     margin: 0;
     padding: 0;
-    border: 0;
-    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
   }
-  #menu-toggle:checked ~ .menu li {
-    border: 1px solid #ffffff00;
-    height: 1.5em;
-    padding: 0.5em;
-    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
-    background-color:#dbf0d2
-  }
+
   .menu > li {
-  
-    justify-content:right;
-    width: 30%;
-    color: white;
+    margin: 0 1rem;
+    overflow: hidden;
   }
-  .menu > li:not(:last-child) {
-    border-bottom: 1px solid #444;
+
+  .menu-button-container {
+    display: none;
+    height: 100%;
+    width: 30px;
+    cursor: pointer;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
- 
-}
+
+  #menu-toggle {
+    display: none;
+  }
+
+  .menu-button,
+  .menu-button::before,
+  .menu-button::after {
+    display: block;
+    background-color: #fff;
+    position: absolute;
+    height: 4px;
+    width: 30px;
+    transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+    border-radius: 2px;
+  }
+
+  .menu-button::before {
+    content: "";
+    margin-top: -8px;
+  }
+
+  .menu-button::after {
+    content: "";
+    margin-top: 8px;
+  }
+
+  #menu-toggle:checked + .menu-button-container .menu-button::before {
+    margin-top: 0px;
+    transform: rotate(405deg);
+  }
+
+  #menu-toggle:checked + .menu-button-container .menu-button::after {
+    margin-top: 0px;
+    transform: rotate(-405deg);
+  }
+
+  @media (max-width: 700px) {
+    .menu-button-container {
+      display: flex;
+    }
+
+    .menu {
+      position: absolute;
+      top: 0;
+      margin-top: 50px;
+      left: 0;
+      flex-direction: column;
+      width: 100%;
+      align-items: end;
+    }
+
+    #menu-toggle ~ .menu li {
+      height: 0;
+      margin: 0;
+      padding: 0;
+      border: 0;
+      transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+    }
+
+    #menu-toggle:checked ~ .menu li {
+      border: 1px solid #ffffff00;
+      height: 1.5em;
+      padding: 0.5em;
+      transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+      background-color:#dbf0d2
+    }
+
+    .menu > li {
+    
+      justify-content:right;
+      width: 30%;
+      color: white;
+    }
+
+    .menu > li:not(:last-child) {
+      border-bottom: 1px solid #444;
+    }
+  }
 </style>
