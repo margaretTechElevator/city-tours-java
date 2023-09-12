fbd6bf8ba5098bc5ee17492e1bfd14e2c7097ee3
Alexander Stuart <ahdisease@gmail.com>
2023-09-12 16:28:36 -0400

    updated landmark info component to take in props

--- vue/src/components/LandmarkInfo.vue
+++ vue/src/components/LandmarkInfo.vue
@@ -1,16 +1,60 @@
 <template>
     <div>
-        hi
+        <h1>{{ landmarkName }}</h1>
+        <div>
+            <img v-bind:src="photos[0].getUrl()">
+            <aside v-for="attribution in photos[0].html_attributions" v-bind:key="attribution.id">
+                Photo Credit: <span v-html="attribution"></span><br>
+            </aside>
+        </div>
+        <section class="contact-details">
+            <p>{{ address }}</p>
+            <p v-show="phoneNumber">{{ phoneNumber }}</p>
+            <a v-show="website">Visit the Website</a>
+        </section>
     </div>
 </template>
 
 <script>
+
+
 export default {
-    name: 'LandmarkInfo'
+    name: 'LandmarkInfo',
+    props: {
+        landmarkName: String,
+        placeId: String,
+        address: String,
+        reviews: Object,
+        photos: {
+            type: Array,
+            default: () => [{
+                getUrl: () => ''
+            }]
+        },
+        phoneNumber: String,
+        ratings: Object,
+        website: String
+    },
+    methods: {
+        defaultUrl() {
+            return [
+                {
+                    getUrl: () => ''
+                }
+            ]
+        }
+    }
 }
 
 </script>
 
 <style scoped>
+h1 {
+    font-size: medium;
+    color: brown;
+}
 
+img {
+    max-width: 50%;
+}
 </style>
