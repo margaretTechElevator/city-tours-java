4ba298ce66f4cd3012d47cda36984ee201bbcca8
Shannon <shannonsandlin@gmail.com>
2023-09-13 19:14:24 -0500

    desgin styles for elements on the main app

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -9,3 +9,14 @@
     name: 'App',
   }
 </script>
+<style >
+div{
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+input{
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+button{
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+</style>
