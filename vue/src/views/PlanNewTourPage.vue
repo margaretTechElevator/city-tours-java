c2998bafedc6ef0b30a9cf7619fb78f64f42850c
Sam Warner <its.sam.warner@gmail.com>
2023-09-15 19:54:59 -0400

    Reroute landing page to become the new itinerary page

--- vue/src/views/PlanNewTourPage.vue
+++ vue/src/views/PlanNewTourPage.vue
@@ -1,9 +1,16 @@
 <template>
   <body>
-    <Navigation />
-    <google-map id="mapElement"/>
-    <TripsTable />
-    <Route/>
+    <div id="back">
+      <Navigation />
+      <div v-if="landing" id="tagline">
+        <div>your city,</div>
+        <div>your adventure,</div>
+        <div>perfectly planned.</div>
+      </div>
+      <google-map />
+      <TripsTable />
+      <Route/>
+    </div>
   </body>
 </template>
   
@@ -14,12 +21,36 @@
   import TripsTable from '../components/TripsTable.vue';
 
   export default {
+    name: 'PlanNewTourPage',
     components: { 
       Navigation, 
       GoogleMap, 
       TripsTable, 
       Route 
     },
-    name: 'PlanNewTourPage',
+    computed: {
+      landing() {
+        return this.$route.path == "/"
+      }
+    }
   }
 </script>
+
+<style>
+  #back { 
+    background-image: url("https://i.ibb.co/TKxNQrR/ny-Opacity.png");
+  }
+
+  #tagline {
+    margin: auto;
+    width: 80%;
+    text-align: center;
+    text-shadow: 1px 1px 15px rgba(215, 255, 53, 0.277);
+    font-size: 2.5rem;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-weight: 900;
+    color: rgb(1, 37, 105);
+    padding-top: 0px;
+    padding-top: 20px;
+  }
+</style>
