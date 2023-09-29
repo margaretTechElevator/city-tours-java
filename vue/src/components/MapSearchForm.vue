9fd5c242e0b3c89654bc2c7ff3568efc05b0a534
Sam Warner <its.sam.warner@gmail.com>
2023-09-29 14:12:14 -0400

    Correct whitespace on new search form

--- vue/src/components/MapSearchForm.vue
+++ vue/src/components/MapSearchForm.vue
@@ -52,10 +52,10 @@ export default {
 
 <style>
   input {
-    width:300px;
-    margin-top:0px;
+    width: 300px;
+    margin-top: 0px;
     margin-left: auto;
-    margin-right:auto;
+    margin-right: auto;
     text-align: center;
     width: 50%;
     box-shadow: 1px 1px 10px rgba(255, 255, 255, 0.36);
@@ -63,59 +63,60 @@ export default {
     background-color: rgba(158, 158, 158, 0.248);
   }
 
-    #searchArea {
-        margin: auto;
-        width: 50%;
-        grid-area: inputs;
-        background-color: rgb(255, 255, 255);
-        padding-bottom: 40px;;
-    }
+  #searchArea {
+    margin: auto;
+    width: 50%;
+    grid-area: inputs;
+    background-color: rgb(255, 255, 255);
+    padding-bottom: 40px;
+  }
 
-    .inputStartingMiles {
-        width:300px;
-        margin-top:10px;
-        margin-left: auto;
-        margin-right:auto;
-        text-align: center;
-        width: 100%;
-        box-shadow: 1px 1px 10px rgba(255, 255, 255, 0.36);
-        border:rgb(203, 203, 203) 0.5px solid;
-        background-color: rgba(158, 158, 158, 0.248);
-    }
-        
-    .grid {
-        display:grid;
-        grid-template-columns: 500px 100px 100px 100px ;
-        grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
-        grid-template-areas: 
-        "startingAddress startingAddress startingAddress startingAddress",
-        "milesFrom milesFrom milesFrom milesFrom",
-        " . whatToSearch whatToSearch .",
-        "radio radio radio radio",
-        ". letsGo letsGo .";
-    }
+  .inputStartingMiles {
+    width: 300px;
+    margin-top: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    width: 100%;
+    box-shadow: 1px 1px 10px rgba(255, 255, 255, 0.36);
+    border:rgb(203, 203, 203) 0.5px solid;
+    background-color: rgba(158, 158, 158, 0.248);
+  }
+      
+  .grid {
+    display: grid;
+    grid-template-columns: 500px 100px 100px 100px;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+    grid-template-areas: 
+    "startingAddress startingAddress startingAddress startingAddress",
+    "milesFrom milesFrom milesFrom milesFrom",
+    ". whatToSearch whatToSearch .",
+    "radio radio radio radio",
+    ". letsGo letsGo .";
+  }
 
-    #milesFrom {
-        grid-area: milesFrom;
-    }
+  #startingAddress {
+    grid-area: startingAddress;
+  }
 
-    #startingAddress {
-        grid-area: startingAddress;
-    }
-    
-    #whatToSearch {
-        grid-area: whatToSearch;
-        text-align: center;
-        padding-top: 20px;
-        padding-bottom: 15px;
-        color: #6b1717;
-        font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
-        font-weight: 900;
-        font-size: 1.5rem;
-        line-height: 0;
-        text-shadow: 1px 1px 10px rgba(130, 114, 110, 0.5);
-    }
-    #letsGo {
-        grid-area: letsGo;
-    }
+  #milesFrom {
+    grid-area: milesFrom;
+  }
+  
+  #whatToSearch {
+    grid-area: whatToSearch;
+    text-align: center;
+    padding-top: 20px;
+    padding-bottom: 15px;
+    color: #6b1717;
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+    font-weight: 900;
+    font-size: 1.5rem;
+    line-height: 0;
+    text-shadow: 1px 1px 10px rgba(130, 114, 110, 0.5);
+  }
+
+  #letsGo {
+    grid-area: letsGo;
+  }
 </style>
