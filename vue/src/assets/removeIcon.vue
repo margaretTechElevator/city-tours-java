19fc05d764a9caf2a5b929a31a948869ece28dfb
Shannon <shannonsandlin@gmail.com>
2023-10-22 20:04:04 -0500

    a few additions

---
+++ vue/src/assets/removeIcon.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6 9C5.44772 9 5 9.44772 5 10V10C5 10.5523 5.44772 11 6 11H14C14.5523 11 15 10.5523 15 10V10C15 9.44772 14.5523 9 14 9H6ZM10 0C4.475 0 0 4.475 0 10C0 15.525 4.475 20 10 20C15.525 20 20 15.525 20 10C20 4.475 15.525 0 10 0ZM10 18C5.59 18 2 14.41 2 10C2 5.59 5.59 2 10 2C14.41 2 18 5.59 18 10C18 14.41 14.41 18 10 18Z" fill="#CFE7CA"/>
+</svg>
+
+</template>
+<script>
+export default{
+    name: "removeIcon",
+}
+</script>
+<style scoped>
+
+</style>
